eb07461cc6d353358f188d915fd1ac036b81b7d5
Tarun-Kumar-hub <ctarun301@gmail.com>
2024-11-23 16:23:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -255,7 +255,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/do",
+      callbackURL: "https://email-scheduler-yuei.onrender.com/auth/google/do",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     async (accessToken, refreshToken, profile, cb) => {
